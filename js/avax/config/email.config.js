43ad208133c885f737db43ef2c1d782d14950569
dewyze <dewyze@users.noreply.github.com>
2022-04-03 21:35:12 +0800

    delete SSLv3

--- js/avax/config/email.config.js
+++ js/avax/config/email.config.js
@@ -6,9 +6,6 @@ module.exports = {
             host: 'smtp-mail.outlook.com', // hostname
             secureConnection: false, // TLS requires secureConnection to be false
             port: 587, // port for secure SMTP
-            tls: {
-                ciphers: 'SSLv3',
-            },
             auth: {
                 user: process.env.EMAIL_USER, // please use outlook, high delivery success rate
                 pass: process.env.EMAIL_PASS,
