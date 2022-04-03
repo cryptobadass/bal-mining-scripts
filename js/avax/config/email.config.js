a13970590db15b580574bb3155fb1ca51e721622
dewyze <dewyze@users.noreply.github.com>
2022-04-03 22:11:50 +0800

    update config

--- js/avax/config/email.config.js
+++ js/avax/config/email.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mail: {
         from: {
             host: 'smtp-mail.outlook.com', // hostname
-            secure: false, // TLS requires secureConnection to be false
+            secureConnection: false, // TLS requires secureConnection to be false
             port: 587, // port for secure SMTP
             auth: {
                 user: process.env.EMAIL_USER, // please use outlook, high delivery success rate
