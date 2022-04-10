3903933b04944c3340187763abcfc7a2d28dee92
dewyze <dewyze@users.noreply.github.com>
2022-04-10 22:26:52 +0800

    update .env.example

--- js/avax/config/email.config.js
+++ js/avax/config/email.config.js
@@ -7,8 +7,8 @@ module.exports = {
             secureConnection: false, // TLS requires secureConnection to be false
             port: 587, // port for secure SMTP
             auth: {
-                user: process.env.EMAIL_USER, // please use outlook, high delivery success rate
-                pass: process.env.EMAIL_PASS,
+                user: process.env.OUTLOOK_EMAIL_USER, // please use outlook, high delivery success rate
+                pass: process.env.OUTLOOK_EMAIL_PASS,
             },
         },
         to: [process.env.EMAIL_RECEIVER],
